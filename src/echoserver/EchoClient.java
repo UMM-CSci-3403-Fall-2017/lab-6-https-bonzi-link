053ab7bd88d6570f76830dc66af6eba774eb95fe
holma198 <holma198@morris.umn.edu>
2017-10-24 11:51:29 -0500

    Start on both the echo and server code, first by implementing the given data server code, then add an input stream and finally an empty array of bytes to store the stream of bytes for returning to the client.  Will need to be finished.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,33 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoClient {
+
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("Serenity.morris.umn.edu", 6013);
+            // You can change 127.0.0.1 to a machine name if you want to try this across
+            // the network to another machine.
+            // Socket socket = new Socket("some.machine.edu", 6013);
+            File file = new File(args[1]);
+            InputStream fileInput = new FileInputStream(file);
+            byte[] bytes = new byte[1024];
+            OutputStream out = socket.getOutputStream();
+
+
+            int count;
+            while((count = fileInput.read(bytes))>0){
+                out.write(bytes,0,count);
+            }
+
+            out.close();
+            fileInput.close();
+            socket.close();
+        } catch (IOException ioe) {
+            System.out.println("We caught an exception");
+            System.err.println(ioe);
+        }
+    }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,28 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoServer {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket sock = new ServerSocket(6013);
+            byte[] bytes = new byte[1024];
+            //InputStream fileInput = sock.getI
+            while (true) {
+                System.out.println("Got a request!");
+                Socket client = sock.accept();
+
+                PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+
+                writer.println(new java.util.Date().toString());
+
+                client.close();
+            }
+
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        }
+    }
 }

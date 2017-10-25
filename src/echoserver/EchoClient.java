dea7bde953e0a31fc58e7fd7ab9ef1ae7519c9bb
holma198 <holma198@morris.umn.edu>
2017-10-25 15:48:45 -0500

    Work on EchoClient.java so that it takes two file arguments

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -7,25 +7,33 @@ public class EchoClient {
 
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("Serenity.morris.umn.edu", 6013);
+            Socket socket = new Socket("Acrylic.morris.umn.edu", 6013);
             // You can change 127.0.0.1 to a machine name if you want to try this across
             // the network to another machine.
             // Socket socket = new Socket("some.machine.edu", 6013);
-            File file = new File(args[0]);
-            InputStream fileInput = new FileInputStream(file);
-            byte[] bytes = new byte[1024*128];
-            OutputStream out = socket.getOutputStream();
 
 
-            int count;
-            while((count = fileInput.read(bytes))>0){
-                out.write(bytes,0,count);
-                System.out.println(socket.getInputStream().toString());
-            }
 
-            out.close();
-            fileInput.close();
-            socket.close();
+                File file = new File(args[0]);
+                System.out.println(file.toString());
+                File output = new File(args[1]);
+                System.out.println(output.toString());
+                InputStream fileInput = new FileInputStream(file);
+                byte[] bytes = new byte[1024 * 128];
+                OutputStream out = socket.getOutputStream();
+
+
+                int count;
+                while ((count = fileInput.read(bytes)) > 0) {
+                    out.write(bytes, 0, count);
+                    System.out.println(socket.getInputStream().toString());
+                }
+
+
+                out.close();
+                fileInput.close();
+                socket.close();
+
         } catch (IOException ioe) {
             System.out.println("We caught an exception");
             System.err.println(ioe);

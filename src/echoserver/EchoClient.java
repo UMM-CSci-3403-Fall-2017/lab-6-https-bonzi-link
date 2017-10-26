ed9db129fd944beacec3bef2e93f96cdf9d9bbf0
bushw011 <bushw011@morris.umn.edu>
2017-10-26 11:50:52 -0500

    Work on EchoClient to take proper standard input in the form of a byte. Also changed EchoServer so that it checks to make sure a byte has been passed in.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,37 +1,46 @@
 package echoserver;
 
+
+
 import java.net.*;
 import java.io.*;
+
 
 public class EchoClient {
 
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("Acrylic.morris.umn.edu", 6013);
+            Socket socket = new Socket("spacewhale.morris.umn.edu", 6013);
             // You can change 127.0.0.1 to a machine name if you want to try this across
             // the network to another machine.
             // Socket socket = new Socket("some.machine.edu", 6013);
 
 
-
-                File file = new File(args[0]);
-                System.out.println(file.toString());
-                File output = new File(args[1]);
-                System.out.println(output.toString());
-                InputStream fileInput = new FileInputStream(file);
-                byte[] bytes = new byte[1024 * 128];
+                //byte bytes[] = new byte[1024*64];
+                int myByte;
+                BufferedReader reader;
+                InputStream readerStream = System.in;
                 OutputStream out = socket.getOutputStream();
-
-
-                int count;
-                while ((count = fileInput.read(bytes)) > 0) {
-                    out.write(bytes, 0, count);
-                    System.out.println(socket.getInputStream().toString());
+                String line;
+                while((myByte = readerStream.read()) != -1){
+                    reader = new BufferedReader(new InputStreamReader(readerStream));
+                    if((line = reader.readLine()) != null){
+                        System.out.println(line);
+                    }
+                    out.write((byte)myByte);
                 }
 
 
+
+                /*while ((count = fileInput.read(bytes)) > 0) {
+                    out.write(bytes, 0, count);
+
+
+                }*/
+
+
                 out.close();
-                fileInput.close();
+
                 socket.close();
 
         } catch (IOException ioe) {
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -10,24 +10,19 @@ public class EchoServer {
     public static void main(String[] args) {
         try {
             ServerSocket serverSock = new ServerSocket(6013);
-            byte[] bytes = new byte[1024*128];
+
             //InputStream fileInput = sock.getI
-            while (true) {
+            Socket client = serverSock.accept();
+            InputStream inStream = client.getInputStream();
+            OutputStream outStream = client.getOutputStream();
+            while (inStream.read() != -1) {
                 System.out.println("Got a request!");
-                Socket client = serverSock.accept();
-                InputStream inStream = client.getInputStream();
-                OutputStream outStream = client.getOutputStream();
                 int count;
-                while((count = inStream.read(bytes)) > 0) {
-                    outStream.write(bytes, 0, count);
-                }
 
-
-                //PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
-                //writer.println(new java.util.Date().toString());
-
-                client.close();
+                PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+                writer.println(inStream.read());
             }
+            client.close();
 
         } catch (IOException ioe) {
             System.err.println(ioe);
